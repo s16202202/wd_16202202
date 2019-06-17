72bd9d383eb3cb28c01a9dab0952a727d6cd19eb
s16202202 <1045418720@qq.com>
2019-06-17 08:51:02 +0800

    课堂测验

---
+++ text/src/main/java/webadv/s16202202/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package webadv.s16202202.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String test() {
+		return "index";
+	}
+
+}
